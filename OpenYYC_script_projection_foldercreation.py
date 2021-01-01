e085635d293c3162eedde12ea36b5f235f2d51d7
T. Yeomans <59377701+tmyeomans@users.noreply.github.com>
2021-01-01 13:30:21 -0700

    updated folder creation script added

--- OpenYYC_script_projection_foldercreation.py
+++ OpenYYC_script_projection_foldercreation.py
@@ -26,7 +26,7 @@ class vecMetadata:
 
         #Build a while loop to ask for a valid workspace location.  Keep looping until a valid location is provided
         while True:
-            arcpy.env.workspace = (r"C:\OpenCalgary_project\OpenCalgary_shapes")
+            arcpy.env.workspace = (r"C:\OpenCalgary_project\Main_shapes")
             if arcpy.Exists(arcpy.env.workspace):
                 print("Workspace location is valid\n")
                 break
@@ -34,8 +34,8 @@ class vecMetadata:
                 print ("Workspace does not exist.  Please enter a valid workspace\n")
 
 
-    #define a function to get the data from each vector file in the workspace
-    def getVecdata (self):
+    #define a function to project each vector file in the workspace
+    def projectVecdata (self):
 
         #use ListFeatureClasses and assign the list to a variable
         self.fcs = arcpy.ListFeatureClasses()
@@ -49,38 +49,47 @@ class vecMetadata:
         for fc in self.fcs:
             geometries = arcpy.CopyFeatures_management(fc, arcpy.Geometry())
             self.spatial_ref = arcpy.Describe(fc).spatialReference
-            #if the spatial reference is unknown, let the user know that is was undefined, and assign it to a new projection
+
+##            #if the spatial reference is unknown, let the user know that is was undefined, and assign it to a new projection
             if self.spatial_ref.name =="Unknown":
                 print ("{}:Unknown Spatial Reference".format(fc))
                 sr = arcpy.SpatialReference(3780)
                 arcpy.DefineProjection_management(fc,sr)
 
-            #if the file has a spatial reference already defined, print out the name of the file and the spatial reference system
+            #if the file has a spatial reference already defined, project it to the new coordinate system
             else:
                 desc = arcpy.Describe(fc)
 
                 sr = arcpy.SpatialReference(3780)
-                fc_proj = os.path.join(r"C:\OpenCalgary_project\Opencalgary_projectedshapes", os.path.splitext(fc)[0])
+                fc_proj = os.path.join(r"C:\OpenCalgary_project\Main_shapes_projected", os.path.splitext(fc)[0])
                 arcpy.Project_management(fc, fc_proj, sr)
-                print("{}:{}".format(fc,self.spatial_ref.name))
+                print("{}".format(fc))
+
+    #define a function to separate features classes into folders based on geometry
+    def buildShapeFolders(self):
+
+        #provide new workspace where the projected files are located
+        arcpy.env.workspace = (r"C:\OpenCalgary_project\Main_shapes_projected")
+        self.fcs = arcpy.ListFeatureClasses()
 
         for fc in self.fcs:
+                desc = arcpy.Describe(fc)
                 if desc.shapeType == "Polygon":
-                    poly_outfc = os.path.join(r"C:\OpenCalgary_project\OpenCalgary_polygons", os.path.splitext(fc_proj)[0])
-                    arcpy.CopyFeatures_management(fc_proj, poly_outfc)
+                    poly_outfc = (r"C:\OpenCalgary_project\OpenCalgary_polygons" + "\\" + fc)
+                    arcpy.CopyFeatures_management(fc, poly_outfc)
 
 
                 if desc.shapeType == "Point":
-                    point_outfc = os.path.join(r"C:\OpenCalgary_project\OpenCalgary_points", os.path.splitext(fc_proj[0]))
-                    arcpy.CopyFeatures_management(fc_proj, point_outfc)
+                    point_outfc = (r"C:\OpenCalgary_project\OpenCalgary_points" + "\\" + fc)
+                    arcpy.CopyFeatures_management(fc, point_outfc)
 
                 if desc.shapeType == "Polyline":
-                    polyline_outfc = os.path.join(r"C:\OpenCalgary_project\OpenCalgary_polylines", os.path.splitext(fc_proj)[0])
-                    arcpy.CopyFeatures_management(fc_proj, polyline_outfc)
+                    polyline_outfc = (r"C:\OpenCalgary_project\OpenCalgary_polylines" + "\\" + fc)
+                    arcpy.CopyFeatures_management(fc, polyline_outfc)
 
                 if desc.shapeType == "Multipoint":
-                    multipoint_outfc = os.path.join(r"C:\OpenCalgary_project\OpenCalgary_multipoint", os.path.splitext(fc_proj)[0])
-                    arcpy.CopyFeatures_management(fc_proj, multipoint_outfc)
+                    multipoint_outfc = (r"C:\OpenCalgary_project\OpenCalgary_multipoint" + "\\" + fc)
+                    arcpy.CopyFeatures_management(fc, multipoint_outfc)
 
                 else:
                     pass
@@ -91,6 +100,7 @@ folderx = vecMetadata()
 
 #call the getVecdata method on folderx
 folderx.getWorkspace()
-folderx.getVecdata()
+folderx.projectVecdata()
+folderx.buildShapeFolders()
 
 
